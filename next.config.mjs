12a00381a26e1aafb91c51f848e8d8d85fdf61d9
DevopsSeries-lab <Devops@Practical.com>
2025-08-28 01:27:45 +0530

    Change Next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      unoptimized: true, // disables image optimization
+    },
+};
 
 export default nextConfig;
